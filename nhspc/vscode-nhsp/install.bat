7b0e2eab3a869695a6985c7472b628912c1493bf
inovia <github@hinekure.net>
2026-04-11 23:58:11 +0900

    install.bat: Pdb2PortablePdb + 全依存DLLをコピーするよう修正

--- nhspc/vscode-nhsp/install.bat
+++ nhspc/vscode-nhsp/install.bat
@@ -34,16 +34,17 @@ echo     拡張ファイル: OK
 set "COMPILER_DIR=%TARGET%\compiler"
 mkdir "%COMPILER_DIR%" 2>nul
 set "NHSPC_FOUND=0"
+set "PDB_FOUND=0"
 
 for %%I in ("%SCRIPT_DIR%\..") do set "NHSPC_ROOT=%%~fI"
 
+:: nhspc.exe
 if exist "%NHSPC_ROOT%\nhspc\bin\Debug\nhspc.exe" (
     echo [*] コンパイラをコピーしています [Debug]
     copy /Y "%NHSPC_ROOT%\nhspc\bin\Debug\nhspc.exe" "%COMPILER_DIR%\" >nul
     copy /Y "%NHSPC_ROOT%\nhspc\bin\Debug\NhspCompiler.Core.dll" "%COMPILER_DIR%\" >nul
     set "NHSPC_FOUND=1"
 )
-
 if exist "%NHSPC_ROOT%\nhspc\bin\Release\nhspc.exe" (
     echo [*] コンパイラをコピーしています [Release]
     copy /Y "%NHSPC_ROOT%\nhspc\bin\Release\nhspc.exe" "%COMPILER_DIR%\" >nul
@@ -51,20 +52,11 @@ if exist "%NHSPC_ROOT%\nhspc\bin\Release\nhspc.exe" (
     set "NHSPC_FOUND=1"
 )
 
-:: Pdb2PortablePdb (PDB変換ツール)
-set "PDB_FOUND=0"
+:: Pdb2PortablePdb + 全依存DLL
 if exist "%NHSPC_ROOT%\Pdb2PortablePdb\bin\Debug\net48\Pdb2PortablePdb.exe" (
     echo [*] PDB変換ツールをコピーしています。
     copy /Y "%NHSPC_ROOT%\Pdb2PortablePdb\bin\Debug\net48\Pdb2PortablePdb.exe" "%COMPILER_DIR%\" >nul
-    copy /Y "%NHSPC_ROOT%\Pdb2PortablePdb\bin\Debug\net48\Microsoft.DiaSymReader.*.dll" "%COMPILER_DIR%\" >nul 2>nul
-    :: amd64 native DLL
-    for /R "%USERPROFILE%\.nuget\packages\microsoft.diasymreader.native" %%F in (Microsoft.DiaSymReader.Native.amd64.dll) do (
-        copy /Y "%%F" "%COMPILER_DIR%\" >nul 2>nul
-    )
-    :: x86 native DLL
-    for /R "%USERPROFILE%\.nuget\packages\microsoft.diasymreader.native" %%F in (Microsoft.DiaSymReader.Native.x86.dll) do (
-        copy /Y "%%F" "%COMPILER_DIR%\" >nul 2>nul
-    )
+    copy /Y "%NHSPC_ROOT%\Pdb2PortablePdb\bin\Debug\net48\*.dll" "%COMPILER_DIR%\" >nul 2>nul
     set "PDB_FOUND=1"
 )
 
@@ -74,16 +66,11 @@ echo   インストール完了
 echo ========================================
 echo.
 echo   拡張:      %TARGET%
-if "!NHSPC_FOUND!"=="1" (
-    echo   コンパイラ: %COMPILER_DIR%\nhspc.exe
-)
-if "!PDB_FOUND!"=="1" (
-    echo   PDB変換:   %COMPILER_DIR%\Pdb2PortablePdb.exe
-)
+if "!NHSPC_FOUND!"=="1" echo   コンパイラ: %COMPILER_DIR%\nhspc.exe
+if "!PDB_FOUND!"=="1" echo   PDB変換:   %COMPILER_DIR%\Pdb2PortablePdb.exe
 echo.
 if "!NHSPC_FOUND!"=="1" (
     echo   VS Code で .nhsp を開いて F5 でコンパイルできます。
-    echo   パス設定は不要です。
 ) else (
     echo   コンパイラが見つかりません。先にビルドしてください:
     echo     cd nhspc
